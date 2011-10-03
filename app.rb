88d5f34faf6d740693396555cdf87fcf0162436f
marutanm <marutanm@gmail.com>
2011-10-03 22:38:11 +0900

    Add routing GET '/'

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'open-uri'
 
+BASE_URL = 'http://devcenter.heroku.com/'
+
+get '/' do
+  doc = Nokogiri::HTML(open(BASE_URL))
+  @contents = doc.xpath('//section[@id="main"]').to_s
+  haml :articles
+end
+
 get '/articles/:uri' do
   doc = Nokogiri::HTML(open("http://devcenter.heroku.com/articles/#{params[:uri]}"))
   @contents = doc.xpath('//section[@id="main"]').to_s
