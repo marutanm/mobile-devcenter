d6adc8ae1653886a320317cca715d9db618a5fc0
marutanm <marutanm@gmail.com>
2011-10-05 12:51:30 +0900

    set data-role=button to link to tag

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ helpers do
   def scraping(url)
     doc = Nokogiri::HTML(open(url)).xpath('//section[@id="main"]')
     doc.xpath('//ul').each { |ul| ul['data-role'] = 'listview' }
+    doc.xpath('//hgroup/p[@class="tags"]/a').each do |a|
+      a['data-role'] = 'button'
+      a['data-inline'] = 'true'
+    end
     @header = doc.xpath('//hgroup/h1').remove.to_s
     @content = doc.inner_html
     nil
